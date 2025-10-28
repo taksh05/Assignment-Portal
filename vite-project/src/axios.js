dd991671f710e481f53b5eefaf50039356655747
taksh05 <takshpaneri05@gmail.com>
2025-10-28 19:17:00 +0530

    Updated API base URL to point to Vercel backend

--- vite-project/src/axios.js
+++ vite-project/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://assignment-portal-86z6.vercel.app/api", // <- YOUR BACKEND URL
+  baseURL: "https://assignment-portal-86z6.vercel.app/", // <- YOUR BACKEND URL
   withCredentials: false, // keep false unless you use cookies
 });
 
