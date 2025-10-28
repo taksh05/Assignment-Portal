b631e698540b3c8c863542908a542a786c19283d
taksh05 <takshpaneri05@gmail.com>
2025-10-28 21:50:24 +0530

    Connected frontend to live backend

--- vite-project/src/axios.js
+++ vite-project/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://assignment-portal-86z6.vercel.app/api", // <- YOUR BACKEND URL
-  withCredentials: false, // keep false unless you use cookies
+const API = axios.create({
+  baseURL: "https://assignment-portal-86z6.vercel.app/api",
 });
+
 
 export default api;
